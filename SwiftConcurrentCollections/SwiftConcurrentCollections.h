bd399646ac5ae73c632ded01b4106d8b75a96075
Peter Prokop <peter.prokop@blackswan.com>
2020-02-03 14:42:52 +0000

    First magical commit

---
+++ SwiftConcurrentCollections/SwiftConcurrentCollections.h
@@ -0,0 +1,19 @@
+//
+//  SwiftConcurrentCollections.h
+//  SwiftConcurrentCollections
+//
+//  Created by Pete Prokop on 03/02/2020.
+//  Copyright © 2020 Pete Prokop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftConcurrentCollections.
+FOUNDATION_EXPORT double SwiftConcurrentCollectionsVersionNumber;
+
+//! Project version string for SwiftConcurrentCollections.
+FOUNDATION_EXPORT const unsigned char SwiftConcurrentCollectionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftConcurrentCollections/PublicHeader.h>
+
+
---
+++ SwiftConcurrentCollectionsTests/SwiftConcurrentCollectionsTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftConcurrentCollectionsTests.swift
+//  SwiftConcurrentCollectionsTests
+//
+//  Created by Pete Prokop on 03/02/2020.
+//  Copyright © 2020 Pete Prokop. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftConcurrentCollections
+
+class SwiftConcurrentCollectionsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
