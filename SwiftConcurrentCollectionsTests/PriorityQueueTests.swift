27736ac5a1a09853a57650cbcc8dc63607a707f3
Petr Prokop <pprokop@bloomberg.net>
2021-03-06 08:14:55 +0000

    Add priority queue

---
+++ SwiftConcurrentCollectionsTests/PriorityQueueTests.swift
@@ -0,0 +1,58 @@
+import XCTest
+@testable import SwiftConcurrentCollections
+
+class PriorityQueueTests: XCTestCase {
+
+    func testOrderingMin() {
+        var x = PriorityQueue<Int>(<)
+
+        x.insert(6)
+        x.insert(5)
+        x.insert(4)
+
+        XCTAssertEqual(x.pop(), 4)
+
+        x.insert(3)
+        x.insert(2)
+        x.insert(1)
+
+        XCTAssertEqual(x.count, 5)
+
+        XCTAssertEqual(x.peek(), 1)
+        XCTAssertEqual(x.pop(), 1)
+        XCTAssertEqual(x.pop(), 2)
+        XCTAssertEqual(x.pop(), 3)
+        XCTAssertEqual(x.pop(), 5)
+        XCTAssertEqual(x.peek(), 6)
+        XCTAssertEqual(x.pop(), 6)
+
+        XCTAssertEqual(x.count, 0)
+    }
+
+    func testOrderingMax() {
+        var x = PriorityQueue<Int>(>)
+
+        x.insert(6)
+        x.insert(5)
+        x.insert(4)
+
+        XCTAssertEqual(x.pop(), 6)
+
+        x.insert(3)
+        x.insert(2)
+        x.insert(1)
+
+        XCTAssertEqual(x.count, 5)
+
+        XCTAssertEqual(x.peek(), 5)
+        XCTAssertEqual(x.pop(), 5)
+        XCTAssertEqual(x.pop(), 4)
+        XCTAssertEqual(x.pop(), 3)
+        XCTAssertEqual(x.pop(), 2)
+        XCTAssertEqual(x.peek(), 1)
+        XCTAssertEqual(x.pop(), 1)
+
+        XCTAssertEqual(x.count, 0)
+    }
+
+}
